759e457443233d6d6df7d4fc72ed79d23116f9e1
Kritthika <55941348+Kritthika@users.noreply.github.com>
2024-12-23 22:56:06 -0800

    Add files via upload

---
+++ SLL_Palindrome Linked List.py
@@ -0,0 +1,30 @@
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution(object):
+    def isPalindrome(self, head):
+        if not head or not head.next:
+            return True
+        slow, fast = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        
+        prev = None
+        while slow:
+            temp = slow.next
+            slow.next = prev
+            prev = slow
+            slow = temp
+        
+       # Compare the two halves
+        first_half, second_half = head, prev
+        while second_half:
+            if first_half.val != second_half.val:
+                return False
+            first_half = first_half.next
+            second_half = second_half.next
+        
+        return True
