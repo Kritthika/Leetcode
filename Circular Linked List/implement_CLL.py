77aeda8d3ea01ccbf3bcbfacc2ee4289e807a6be
Kritthika <55941348+Kritthika@users.noreply.github.com>
2024-12-27 22:50:59 -0800

    workingCLL.py

---
+++ Circular Linked List/implement_CLL.py
@@ -0,0 +1,55 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+    
+    def __str__(self):
+        return str(self.value)
+
+class CSLinkedList:
+    
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+    
+    def __str__(self):
+        temp_node = self.head
+        result =''
+        while temp_node is not None:
+            result += str(temp_node.value)
+            temp_node = temp_node.next
+            if temp_node == self.head:
+                break
+            result += '-->'
+        return result
+    def append(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            new_node.next = self.head
+            self.tail = new_node
+        self.length+=1
+
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head =new_node 
+            self.tail.next = new_node
+        self.length +=1
+
+csllinked = CSLinkedList()
+csllinked.append(10)
+csllinked.append(20)
+csllinked.append(30)
+csllinked.prepend(40)
+
+print(csllinked)
+
